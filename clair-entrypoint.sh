c02f11927b4e9b8385063e0e594bf7969cd467c1
TobiLG <tobilg@gmail.com>
2017-04-04 19:11:39 +0200

    Remove debug output

--- clair-entrypoint.sh
+++ clair-entrypoint.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-# Show environment
-env
-
 # Render config.conf and start postgrest
-dockerize -delims "<%:%>" -template $CLAIR_DCOS_PATH/config.yaml.template:$CLAIR_DCOS_PATH/config.yaml -wait tcp://$POSTGRES_HOST:$POSTGRES_PORT -timeout ${POSTGRES_TIMEOUT_SECONDS}s cat $CLAIR_DCOS_PATH/config.yaml && /clair -config=$CLAIR_DCOS_PATH/config.yaml
+dockerize -delims "<%:%>" -template $CLAIR_DCOS_PATH/config.yaml.template:$CLAIR_DCOS_PATH/config.yaml -wait tcp://$POSTGRES_HOST:$POSTGRES_PORT -timeout ${POSTGRES_TIMEOUT_SECONDS}s /clair -config=$CLAIR_DCOS_PATH/config.yaml
